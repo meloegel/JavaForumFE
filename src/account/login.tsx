956c0f7d1bbf793312fa2ea84f1750fd15c5edc9
meloegel <meloegel@gmail.com>
2021-12-10 21:46:06 -0500

    inital commit

---
+++ src/account/login.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Login():JSX.Element {
+    return (
+        <div>
+            <h1>Fart</h1>
+        </div>
+    )
+
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AuthenticatedRouter from './router/authenticatedRouter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthenticatedRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
