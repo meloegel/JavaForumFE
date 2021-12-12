ed5fa7c35b45d720119dfbc5163fdb5dab9cb5b9
meloegel <meloegel@gmail.com>
2021-12-11 20:50:58 -0500

    working login

--- src/account/login.tsx
+++ src/account/login.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as yup from "yup";
 import Button from "../common/button";
+import useFetch from "../hooks/useFetch";
 import loginSchema from "../validation/loginSchema";
 
 const initialFormValues = {
@@ -21,9 +22,10 @@ export default function Login(): JSX.Element {
   const [formValues, setFormValues] = useState(initialFormValues);
   const [formErrors, setFormErrors] = useState(initialFormErrors);
   const [disabled, setDisabled] = useState(initialDisabled);
+  const [request, data] = useFetch<any>();
 
   const onInputChange = (evt: any) => {
-    const name = evt.target.value;
+    const name = evt.target.name;
     const value = evt.target.value;
     yup
       .reach(loginSchema, name)
@@ -47,10 +49,33 @@ export default function Login(): JSX.Element {
   };
 
   const onSubmit = (evt: any) => {
-    evt.PreventDefault();
+    evt.preventDefault();
+    const body = {
+      grant_type: "password",
+      username: formValues.username,
+      password: formValues.password,
+    };
+    const headers = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${btoa("lambda-client:lambda-secret")}`,
+    };
+    const grant_type = `?grant_type=password&username=${formValues.username}&password=${formValues.password}`;
 
-    navigate("/home");
+    request(`http://localhost:2019/login${grant_type}`, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: headers,
+    });
   };
+
+    useEffect(() => {
+      if (data) {
+        console.log("Success");
+        localStorage.setItem("username", `${formValues.username}`);
+        localStorage.setItem("token", `Bearer ${data.access_token}`);
+        navigate("/home");
+      }
+    }, [data, navigate, formValues]);
 
   useEffect(() => {
     loginSchema.isValid(formValues).then((valid: any) => {
@@ -85,7 +110,16 @@ export default function Login(): JSX.Element {
           </div>
         </div>
         <div>
-          <Button text="Login" disabled={disabled} className="" />
+          <div>{formErrors.username}</div>
+          <div>{formErrors.password}</div>
+        </div>
+        <div>
+          <Button
+            text="Login"
+            disabled={disabled}
+            className=""
+            onClick={() => {}}
+          />
         </div>
       </form>
     </div>
--- src/validation/loginSchema.tsx
+++ src/validation/loginSchema.tsx
@@ -1,8 +1,16 @@
 import * as yup from "yup";
 
 const loginSchema = yup.object().shape({
-  username: yup.string().trim().required("Please enter a valid username"),
-  password: yup.string().trim().required("Please enter your password"),
+  username: yup
+    .string()
+    .trim()
+    .min(4, "Please enter a valid username")
+    .required("Please enter a valid username"),
+  password: yup
+    .string()
+    .trim()
+    .min(4, "Please enter your password")
+    .required("Please enter your password"),
 });
 
 export default loginSchema;
