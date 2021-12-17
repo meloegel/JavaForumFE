1cc1b9914e5ab85f33ab74cb7b778ec9bbd439db
meloegel <meloegel@gmail.com>
2021-12-16 23:11:37 -0500

    get topic working on topicForum

--- src/pages/topicForum.tsx
+++ src/pages/topicForum.tsx
@@ -1,7 +1,60 @@
+import { useEffect, useState } from "react";
+import TopicCard from "../common/topicCard";
+import useFetch from "../hooks/useFetch";
+
+const initialTopicValues = {
+  topicname: "",
+  topicbody: "",
+  topicphoto: "",
+  topicvideo: "",
+  topiclink: "",
+  nsfw: "",
+  user:{
+      username: ""
+  },
+};
+
 export default function TopicForum(): JSX.Element {
+  const [topic, setTopic] = useState(initialTopicValues);
+  const token = window.localStorage.getItem("token");
+  const topicid = window.location.href.split("/").slice(-1)[0];
+  const [getTopic, topicData] = useFetch<any>();
+
+  useEffect(() => {
+    console.log(topicid);
+    if (topicid) {
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: token!,
+      };
+      getTopic(`http://localhost:2019/topics/topic/${topicid}`, {
+        method: "GET",
+        headers: headers,
+      });
+    }
+  }, [token, getTopic, topicid]);
+
+  useEffect(() => {
+    console.log(topicData);
+    if (topicData) {
+      setTopic(topicData);
+    }
+  }, [topicData]);
+
   return (
     <div>
       <h2>Forum</h2>
+      {topic.topicname !== "" ? (
+        <TopicCard
+          topicname={topic.topicname}
+          topicbody={topic.topicbody}
+          topicphoto={topic.topicphoto}
+          topicvideo={topic.topicvideo}
+          topiclink={topic.topiclink}
+          nsfw={JSON.parse(topic.nsfw)}
+          user={topic.user.username}
+        />
+      ) : null}
     </div>
   );
 }
