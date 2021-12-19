bb579dbee7b1d67f26b6d8c481c36684c25ba6fb
meloegel <meloegel@gmail.com>
2021-12-18 20:24:21 -0500

    can get userid on user topic

--- src/account/userTopics.tsx
+++ src/account/userTopics.tsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
+
 export default function UserTopics(): JSX.Element {
+  const [getUserInfo, userInfo] = useFetch<any>();
+  const [userId, setUserId] = useState(0);
+  const username = window.localStorage.getItem("username");
+  const token = window.localStorage.getItem("token");
+
+  useEffect(() => {
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: token!,
+    };
+    getUserInfo(`http://localhost:2019/users/user/name/${username}`, {
+      method: "GET",
+      headers: headers,
+    });
+  }, [getUserInfo, token, username]);
+
+  useEffect(() => {
+    if (userInfo) {
+      setUserId(userInfo.userid);
+    }
+  }, [userInfo]);
+
   return (
     <div>
       <h2>User Topics</h2>
