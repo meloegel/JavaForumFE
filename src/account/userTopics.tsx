420257d3dbc762caf7a1e2793dfe2529557b3db0
meloegel <meloegel@gmail.com>
2021-12-19 23:08:37 -0500

    delete topic semi-working, need to configure backend to delete all comments before deleting topic

--- src/account/userTopics.tsx
+++ src/account/userTopics.tsx
@@ -8,10 +8,13 @@ export default function UserTopics(): JSX.Element {
   const navigate = useNavigate();
   const [getUserInfo, userInfo] = useFetch<any>();
   const [getUserTopics, userTopics] = useFetch<any>();
+  const [deleteTopic, deleteTopicResp] = useFetch<any>();
   const [topics, setTopics] = useState([] as any[]);
   const [userId, setUserId] = useState(0);
   const username = window.localStorage.getItem("username");
   const token = window.localStorage.getItem("token");
+  const [confirm, setConfirm] = useState(false);
+  const [topicid, setTopicid] = useState(0);
 
   const handleOnTopicClick = (id: number) => {
     navigate(`/forum/${id}`);
@@ -19,6 +22,22 @@ export default function UserTopics(): JSX.Element {
 
   const handleOnDeleteClick = (topicid: number) => {
     console.log(topicid);
+    setConfirm(!confirm);
+    setTopicid(topicid);
+  };
+
+  const handleOnDeleteConfirm = () => {
+    console.log(topicid);
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: token!,
+    };
+    deleteTopic(`http://localhost:2019/topics/topic/${topicid}`, {
+      method: "DELETE",
+      headers: headers,
+    });
+    setConfirm(!confirm);
+    // need to configured backend to delete all comments before deleting topic
   };
 
   useEffect(() => {
@@ -55,9 +74,39 @@ export default function UserTopics(): JSX.Element {
     }
   }, [userTopics]);
 
+  useEffect(() => {
+    if (deleteTopicResp) {
+      window.location.reload();
+    }
+  }, [deleteTopicResp]);
+
   return (
     <div className="">
       <h2 className="text-white text-3xl p-6 text-center">User Topics</h2>
+      <div
+        className={`absolute z-0 m-auto w-full ${
+          confirm ? "visible" : "hidden"
+        } `}
+      >
+        <div className="bg-gray-400 p-8 w-1/2 m-auto">
+          <h2 className="text-center">Alert</h2>
+          <p className="text-center">
+            Are you sure you would like to delete this topic?
+          </p>
+          <div className="flex justify-center">
+            <Button
+              text="Confirm"
+              className="text-white"
+              onClick={() => handleOnDeleteConfirm()}
+            />
+            <Button
+              text="Cancel"
+              className="text-white"
+              onClick={() => setConfirm(!confirm)}
+            />
+          </div>
+        </div>
+      </div>
       {topics !== []
         ? topics.map((topic, key): any => (
             <div className="flex flex-col">
--- src/common/alert.tsx
+++ src/common/alert.tsx
@@ -16,12 +16,18 @@ export default function Alert({
   cancel,
 }: AlertType): JSX.Element {
   return (
-    <div className={` ${visible ? "hidden" : "visible"}`}>
-      <h2>{title}</h2>
-      <p>{body}</p>
-      <div className="flex">
-        <Button text="Confirm" className="text-white" onClick={() => confirm} />
-        <Button text="Cancel" className="text-white" onClick={() => cancel} />
+    <div className={`absolute z-0 m-auto w-full ${visible ? "visible" : "hidden"} `}>
+      <div className="bg-gray-400 p-8 w-1/2 m-auto">
+        <h2 className="text-center">{title}</h2>
+        <p className="text-center">{body}</p>
+        <div className="flex justify-center">
+          <Button
+            text="Confirm"
+            className="text-white"
+            onClick={() => confirm}
+          />
+          <Button text="Cancel" className="text-white" onClick={() => cancel} />
+        </div>
       </div>
     </div>
   );
